dc38e75232f2d4d0ba334a3227d6c314b68459e0
Jeswin Kumar <jeswinpk@agilehead.com>
2017-10-15 17:55:01 +0530

    WIP handle all forms of imports

--- src/schemas/rpc.js
+++ src/schemas/rpc.js
@@ -16,47 +16,50 @@ import composite from "../chimpanzee-utils/composite";
 import clean from "../chimpanzee-utils/node-cleaner";
 
 export default function(state, analysisState) {
-  const nested = {
-    type: "MemberExpression",
-    object: source([collection, query])(state, analysisState),
-    property: {
-      type: "Identifier",
-      name: capture("procedure")
-    }
-  };
-
-  const noNested = source([collection])(state, analysisState);
-
-  return composite(
+  const callees = [
     {
-      type: "CallExpression",
-      callee: any([nested, noNested]),
-      arguments: optional(capture("args"))
+      type: "MemberExpression",
+      object: source([collection, query])(state, analysisState),
+      property: {
+        type: "Identifier",
+        name: capture("procedure")
+      }
     },
-    {
-      build: rpc => context => _result =>
-        _result instanceof Match
-          ? (() => {
-              //const result = 
-              if (!result.value.procedure) {
-                // Recalibration in case of no nesting.
-                result.value.object = result.value.callee;
-                delete result.value.callee;
-                result.value.procedure = result.value.object.collection;
-                delete result.value.object.collection;
-              }
+    source([collection])(state, analysisState)
+  ];
 
-              return result.value.arguments[0]
-                ? rpcPost(
-                    result.value.object,
-                    { function: result.value.procedure },
-                    { args: clean(result.value.arguments[0]) }
-                  )
-                : rpcPost(result.value.object, {
-                    function: result.value.procedure
-                  });
-            })()
-          : result
-    }
+  const schemas = callees.map(callee =>
+    composite({
+      type: "CallExpression",
+      callee,
+      arguments: optional(capture("args"))
+    })
   );
+
+  return any(schemas, {
+    build: rpc => context => _result =>
+      _result instanceof Match
+        ? (() => {
+            console.log(_result)
+            //const result =
+            if (!result.value.procedure) {
+              // Recalibration in case of no nesting.
+              result.value.object = result.value.callee;
+              delete result.value.callee;
+              result.value.procedure = result.value.object.collection;
+              delete result.value.object.collection;
+            }
+
+            return result.value.arguments[0]
+              ? rpcPost(
+                  result.value.object,
+                  { function: result.value.procedure },
+                  { args: clean(result.value.arguments[0]) }
+                )
+              : rpcPost(result.value.object, {
+                  function: result.value.procedure
+                });
+          })()
+        : result
+  });
 }
