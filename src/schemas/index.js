c2f8ff1b58003469df56dedf20103323eecbef8d
Jeswin Kumar <jeswinpk@agilehead.com>
2017-10-23 09:54:02 +0530

    WIP

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -8,8 +8,9 @@ import {
   capture,
   builtins as $
 } from "chimpanzee";
-import { source, composite, clean } from "isotropy-analyzer-utils";
+import { source, clean } from "isotropy-analyzer-utils";
 import root from "./root";
+import { composite } from "chimpanzee";
 
 export function memberExpression(state, analysisState) {
   return composite(
@@ -37,14 +38,15 @@ export function memberExpression(state, analysisState) {
 }
 
 export function callExpression(state, analysisState) {
-  return composite(
-    {
-      type: "CallExpression",
-      callee: source(() => [root, wsSchema])(state, analysisState),
-      arguments: capture("arguments")
-    },
-    {
-      build: obj => () => result =>
+  const schema = {
+    type: "CallExpression",
+    callee: source(() => [root, wsSchema])(state, analysisState),
+    arguments: capture("arguments")
+  };
+
+  return composite(schema, {
+    build: buildDefinition(
+      obj => () => result =>
         result instanceof Match
           ? {
               ...result.value.callee,
@@ -53,9 +55,35 @@ export function callExpression(state, analysisState) {
                 arguments: result.value.arguments
               })
             }
+          : result,
+      {
+        schema,
+        parent: s => s.callee,
+        errors: {
+          hasParentButSkipped: () => ``
+        }
+      }
+    )
+  });
+}
+
+function buildDefinition(build, params) {
+  return params
+    ? (obj, key, parent, parentKeys) => context => result =>
+        result instanceof Skip
+          ? params.parent && params.errors && params.errors.hasParentButSkipped
+            ? (() => {
+                const parentVal = parse(parent(schema))(
+                  obj,
+                  key,
+                  parent,
+                  parentKeys
+                )(context);
+
+              })()
+            : result
           : result
-    }
-  );
+    : build;
 }
 
 const wsSchema = function(state, analysisState) {
